eec7a4a3cd312d16d304ef1236958a8213ca01cf
mountaincenter <y.hiroyuki560730@gmail.com>
2023-03-05 10:59:52 +0900

    create terraform vpc subnet

--- variable.tf
+++ variable.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  type = string
+}
+
 variable "tool" {
   type = string
 }
@@ -8,4 +12,18 @@ variable "project" {
 
 variable "environment" {
   type = string
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/21"
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c"]
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["1a", "1c"]
 }
